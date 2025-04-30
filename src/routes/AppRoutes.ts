a6ade16adb3be5f28f04c20995b5089ae20de637
moon8603 <buikimngan2003@gmail.com>
2025-04-30 20:42:42 +0900

    Add Route and Homepage's Skeleton, delete unnecessary files

---
+++ src/routes/AppRoutes.ts
@@ -0,0 +1,6 @@
+export enum AppRoutes {
+  HOME = "/",
+  SEAT_SELECTION_PAGE = "/seat-selection-page",
+  LOGIN_PAGE = "/login-page",
+  SIGN_UP_PAGE = "/sign-up-page",
+}
---
+++ src/routes/MyRoute.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import { AppRoutes } from './AppRoutes';
+import HomePage from '../pages/HomePage';
+
+const MyRoute:React.FC = () => {
+  return (
+    <>
+      <Routes>
+        <Route path={AppRoutes.HOME} element={<HomePage />}/>
+      </Routes>
+    </>
+  )
+}
+
+export default MyRoute
