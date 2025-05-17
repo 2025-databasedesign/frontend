7d8b06a8f7965b0783d607955335e7bfdee1da45
moon8603 <buikimngan2003@gmail.com>
2025-05-18 03:24:54 +0900

    Implement SchedulePage(상영일정 페이지)

--- src/routes/MyRoute.tsx
+++ src/routes/MyRoute.tsx
@@ -4,6 +4,7 @@ import { AppRoutes } from "./AppRoutes";
 import HomePage from "../pages/HomePage";
 import LoginPage from "../pages/LoginPage";
 import SignUpPage from "../pages/SignUpPage";
+import SchedulePage from "../pages/SchedulePage";
 
 const MyRoute: React.FC = () => {
   return (
@@ -12,6 +13,7 @@ const MyRoute: React.FC = () => {
         <Route path={AppRoutes.HOME} element={<HomePage />} />
         <Route path={AppRoutes.LOGIN_PAGE} element={<LoginPage />} />
         <Route path={AppRoutes.SIGN_UP_PAGE} element={<SignUpPage />} />
+        <Route path={AppRoutes.SCHEDULE_PAGE} element={<SchedulePage />} />
       </Routes>
     </>
   );
