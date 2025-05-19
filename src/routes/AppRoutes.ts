a5499165e0ef4221296e54d635a7829fff07c36c
hyogeun <gyrms3785@naver.com>
2025-05-19 18:16:24 +0900

    movie list, detail page 구현중
    movie list, detail page 구현중

--- src/routes/AppRoutes.ts
+++ src/routes/AppRoutes.ts
@@ -3,5 +3,7 @@ export enum AppRoutes {
   SEAT_SELECTION_PAGE = "/seat-selection-page",
   LOGIN_PAGE = "/login-page",
   SIGN_UP_PAGE = "/sign-up-page",
+  MOVIELIST_PAGE = "/movie-list-page",
+  MOVIEDETAIL_PAGE = "/movies/:movieId",
   SCHEDULE_PAGE = "/schedule-page",
 }
